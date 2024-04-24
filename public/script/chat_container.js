a4efd6452acde225d99a12e8219743766695a942
jaehoonV <ljh1032112@gmail.com>
2024-04-24 18:06:36 +0900

    Update Chat

--- public/script/chat_container.js
+++ public/script/chat_container.js
@@ -1,21 +1,15 @@
-let chat_list;
+let master_seq;
 
 function init(){
-    let user = 'aa';
-    let master_seq = 1;
-
     $.ajax({
         url : "/chat_container/",
         type : "POST",
-        dataType : "JSON",
-        data : {"user" : user,
-                "master_seq" : master_seq}
+        dataType : "JSON"
     })
     .done(function (json){
         let json_data = JSON.parse(JSON.stringify(json));
-        chat_list = json_data.results_1;
-        console.log(chat_list);
-        create_chat_container();
+        let chat_list = json_data.results;
+        create_chat_list(chat_list);
     })
     .fail(function (xhr, status, errorThrown){
         alert("Ajax failed")
@@ -24,10 +18,52 @@ function init(){
 
 init();
 
-function create_chat_container(){
+function create_chat_list(chat_list){
+    let chat_result = "";
+    for(let chat of chat_list){
+        let chat_room_name = chat.chat_room_name;
+        let master_seq = chat.master_seq;
+        let member_seq = chat.member_seq;
+        let member_nm = chat.member_nm;
+        let contents = chat.contents;
+        let reg_date = chat.reg_date;
+        chat_result += "<div class='my_chat_list' data-seq='" + master_seq + "'>"
+                    + "<em class='chat_user'>" + member_nm + "</em>" 
+                    + "<em class='chat_summary'>" + contents + "</em>" 
+                    + "</div>"; 
+    }
+
+    $('#chat_list_container').html(chat_result);
+}
+
+$(document).on("click",".my_chat_list",function(e){ 
+    e.preventDefault();
+    master_seq = $(this).data('seq');
+    setChat(master_seq);
+})
+
+function setChat(seq){
+    $('#chat_input').val('');
+    $.ajax({
+        url : "/chat_container/chat_contents",
+        type : "POST",
+        dataType : "JSON",
+        data : {"master_seq" : seq}
+    })
+    .done(function (json){
+        let json_data = JSON.parse(JSON.stringify(json));
+        let chat_contents = json_data.results;
+        create_chat_container(chat_contents);
+    })
+    .fail(function (xhr, status, errorThrown){
+        alert("Ajax failed")
+    })
+}
+
+function create_chat_container(chat_contents){
     let chat_result = "";
 
-    for(let chat of chat_list){
+    for(let chat of chat_contents){
         let reg_day = chat.reg_day;
         let reg_time = chat.reg_time;
         if(chat.mine_div == 'mine'){
@@ -47,5 +83,37 @@ function create_chat_container(){
         chat_result += "</div>";
     }
 
-    $('#chat_container').html(chat_result);
+    $('#chat').html(chat_result);
+    MoveFocus();
+}
+
+function MoveFocus() {
+    $('#chat_input').focus();
+}
+
+$('#chat_input').on("keydown", function(e){
+    if(e.which==13){
+        send_chat();
+    }
+});
+
+function send_chat(){
+    let contents = $('#chat_input').val();
+    let seq = master_seq;
+    $.ajax({
+        url : "/chat_container/insert",
+        type : "POST",
+        dataType : "JSON",
+        data : {"master_seq" : seq,
+                "contents" : contents}
+    })
+    .done(function (json){
+        let json_data = JSON.parse(JSON.stringify(json));
+        let chk = json_data.chk;
+        console.log(chk);
+        setChat(seq);
+    })
+    .fail(function (xhr, status, errorThrown){
+        alert("Ajax failed")
+    })
 }
--- routes/chat_container.js
+++ routes/chat_container.js
@@ -1,37 +1,140 @@
 let express = require('express');
 let router = express.Router();
 
+let createLog = require('../public/script/createLog.js');
+let authCheck = require('../public/script/authCheck.js');
 // mariaDB Connection
 const maria = require('../ext/conn_mariaDB');
 
 router.use(express.static("public"));
 
 router.get('/', (req, res) => {
-    res.render('chat_container');
+  if (!authCheck.isOwner(req, res)) {  // login page
+    res.redirect('/login');
+    return false;
+  } else {
+    let master_yn = {"master_yn" : authCheck.isMaster(req, res)};
+    
+    createLog.insertLog(req, res, 'CHAT PAGE', 'MOVE');
+    res.render('chat_container', master_yn);
+    return false;
+  }
 })
 
-let sql_1 = "select a.contents, a.reg_id, a.reg_date, date_format(a.reg_date, '%y-%c-%e') reg_day, date_format(a.reg_date, '%h:%i') reg_time, "
-            + "case when reg_id = ? then 'mine' else 'other' end mine_div, "
-            + "count(b.member_seq) - count(b.read_date) unread_num "
-            + "from ex1.chat_contents a, ex1.chat_read_history b "
-            + "where a.contents_seq = b.contents_seq and a.master_seq = ? "
-            + "group by a.contents, a.reg_id, a.reg_date "
-            + "order by a.reg_date";
-
 router.post('/', (req, res) => {
+  console.log(req.body);
+  let useremail = authCheck.getUseremail(req, res);
+  let sql = "SELECT b.chat_room_name, b.master_seq, (SELECT USERNAME FROM color_memo.MEMBER WHERE EMAIL = e.user_id) AS member_nm, c.member_seq, d.contents, DATE_FORMAT(d.reg_date, '%Y-%m-%d') AS reg_date "
+          + "FROM ex1.chat_member a "
+          + "RIGHT OUTER JOIN ex1.chat_master b "
+          + "ON a.master_seq = b.master_seq "
+          + "RIGHT OUTER JOIN ex1.chat_member c "
+          + "ON a.master_seq = c.master_seq "
+          + "RIGHT OUTER JOIN ex1.v_chat_last_contents d "
+          + "ON a.master_seq = d.master_seq "
+          + "RIGHT OUTER JOIN ex1.chat_user e "
+          + "ON c.member_seq = e.user_seq "
+          + "WHERE a.member_seq = (SELECT user_seq FROM ex1.chat_user WHERE user_id = ?) "
+          + "AND c.member_seq != (SELECT user_seq FROM ex1.chat_user WHERE user_id = ?) "
+          + "ORDER BY reg_date ";
+  let sql_data;
+  maria.query(sql, [useremail, useremail], function (err, results) {
+    if (err) {
+        console.log(err);
+        res.render('error', {error: err});
+    }
+    sql_data = {
+      "results" : results,
+    }
+    res.json(sql_data);
+  });
+})
+
+router.post('/chat_contents', (req, res) => {
     console.log(req.body);
+    let useremail = authCheck.getUseremail(req, res);
+    let sql = "SELECT a.contents, a.reg_id, a.reg_date, DATE_FORMAT(a.reg_date, '%Y-%m-%d') reg_day, DATE_FORMAT(a.reg_date, '%h:%i') reg_time, "
+            + "CASE WHEN reg_id = ? THEN 'mine' ELSE 'other' END mine_div, "
+            + "COUNT(b.member_seq) - COUNT(b.read_date) unread_num "
+            + "FROM ex1.chat_contents a, ex1.chat_read_history b "
+            + "WHERE a.contents_seq = b.contents_seq AND a.master_seq = ? "
+            + "GROUP BY a.contents, a.reg_id, a.reg_date "
+            + "ORDER BY a.reg_date";
     let sql_data;
-    maria.query(sql_1, [req.body.user, req.body.master_seq], function (err, results) {
+    maria.query(sql, [useremail, req.body.master_seq], function (err, results) {
       if (err) {
           console.log(err);
           res.render('error', {error: err});
       }
       sql_data = {
-        "results_1" : results,
+        "results" : results,
       }
       res.json(sql_data);
     });
   })
+
+router.post('/insert', async (req, res) => {
+  let useremail = authCheck.getUseremail(req, res);
+  try {
+    let chk = 'Y';
+
+    // contents_seq 조회
+    const sql_contents_seq_select = "SELECT NEXTVAL(ex1.contents_seq) AS seq FROM DUAL;";
+    const result = await new Promise((resolve, reject) => {
+      maria.query(sql_contents_seq_select, function (err, result) {
+        if (err) {
+          chk = 'N';
+          reject(err);
+        } else {
+          resolve(result);
+        }
+      });
+    });
+
+    const results = JSON.parse(JSON.stringify(result));
+    const contents_seq = results[0].seq;
+
+    // chat_contents에 데이터 삽입
+    const sql_chat_insert = "INSERT INTO ex1.chat_contents(master_seq, contents_seq, contents, reg_id, reg_date) VALUES (?, ?, ?, ?, CURRENT_TIMESTAMP);";
+    await new Promise((resolve, reject) => {
+      maria.query(sql_chat_insert, [req.body.master_seq, contents_seq, req.body.contents, useremail], function (err, result) {
+        if (err) {
+          chk = 'N';
+          reject(err);
+        } else {
+          // 로그 삽입 및 리다이렉트
+          createLog.insertLog(req, res, 'CHAT INSERTED', 'ACT');
+          resolve(result);
+        }
+      });
+    });
+
+    // chat_read_history에 데이터 삽입
+    const sql_chat_history_insert = "INSERT INTO ex1.chat_read_history(contents_seq, member_seq) SELECT ?, member_seq FROM ex1.chat_member WHERE master_seq = ?;";
+    await new Promise((resolve, reject) => {
+      maria.query(sql_chat_history_insert, [contents_seq, req.body.master_seq], function (err, result) {
+        if (err) {
+          chk = 'N';
+          reject(err);
+        } else {
+          // 로그 삽입 및 리다이렉트
+          createLog.insertLog(req, res, 'CHAT HISTORY INSERTED', 'ACT');
+          resolve(result);
+        }
+      });
+    });
+
+    let done_chk = {
+      "chk" : chk
+    }
+
+    res.json(done_chk);
+
+  } catch (err) {
+    console.log(err);
+    res.render('error', { error: err });
+  }
+})
 
 // 404 Error Handling
 router.all('*',(req, res, next) => {
