2e4b6e9542db50f231400318f4399ed03ae9f425
jaehoon <92502363+jaehoonV@users.noreply.github.com>
2024-06-11 12:29:02 +0900

    Update conn_mariaDB.js

--- ext/conn_mariaDB.js
+++ ext/conn_mariaDB.js
@@ -2,7 +2,7 @@ const maria = require('mysql'); // mysql 모듈 로드
 
 const connection = maria.createConnection({  // mysql 접속 설정
     host: 'svc.gksl2.cloudtype.app',
-    port: '32059',
+    port: '30383',
     user: 'root',
     password: '1032',
     database: 'color_memo',
